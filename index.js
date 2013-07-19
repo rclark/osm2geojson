678479aec06cf3294d5e323a494f9ce548ac785b
Ryan Clark <ryan.clark.j@gmail.com>
2013-07-19 10:09:38 -0700

    setup for npm installation

---
+++ index.js
@@ -0,0 +1,11 @@
+var osm2geojson = require("./osm2geojson"),
+    
+    scriptIndex = process.argv.indexOf("osm2geojson"),
+    left = process.argv[scriptIndex + 1],
+    bottom = process.argv[scriptIndex + 2],
+    right = process.argv[scriptIndex + 3],
+    top = process.argv[scriptIndex + 4];
+
+osm2geojson([left, bottom, right, top], process.stdout, function (err) {
+    process.stderr.write(err);    
+});
